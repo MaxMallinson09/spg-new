b10384cbc9c4e964614a2e01e3bf6028e7b88713
Max Mallinson <116023119+MaxMallinson09@users.noreply.github.com>
2026-08-23 11:29:32 +0100

    Apply suggestion from @MaxMallinson09

--- src/components/PasswordGenerator.tsx
+++ src/components/PasswordGenerator.tsx
@@ -40,7 +40,7 @@ const WORDS = [
   'Mango', 'Map', 'Moon', 'Motor', 'Nest', 'Note', 'Ocean', 'Olive',
   'Onion', 'Oven', 'Paint', 'Panel', 'Paper', 'Park', 'Pasta', 'Pen',
   'Piano', 'Pink', 'Pizza', 'Plant', 'Plate', 'Pond',
-  'Pool', 'Radio', 'Ramp', 'Rice', 'River', 'Robot', 'Rock', 'Room',
+  'Ramp', 'Rice', 'River', 'Robot', 'Rock', 'Room',
   'Ruby', 'Sand', 'Shelf', 'Sink', 'Slide', 'Snow', 'Sock', 'Soup',
   'Star', 'Stone', 'Sugar', 'Table', 'Tag', 'Tent', 'Tile', 'Toast',
   'Token', 'Town', 'Train', 'Tree', 'Video', 'Watch', 'Wire', 'Wool',
